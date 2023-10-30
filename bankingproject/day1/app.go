a4b6bbf08c838fa879d3477d7532d0a0a2e1d83e
parameswari.bala <parameswaribala@gmail.com>
2023-10-30 14:22:42 +0530

    updated

--- bankingproject/day1/app.go
+++ bankingproject/day1/app.go
@@ -26,5 +26,8 @@ func main() {
 	address = "chennai"
 
 	fmt.Println("Rocking with Go!!!")
-	fmt.Printf("Customre Name=%s having accountNo=%d", name, accountNumber)
+	fmt.Printf("Customre Name=%s having accountNo=%d\n", name, accountNumber)
+	//invoking function from external go file in the same package
+	helper()
+
 }
