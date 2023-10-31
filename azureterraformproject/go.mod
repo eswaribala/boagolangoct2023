06d82b951961e1d90a955cb08ef3b4e1f1f79b93
parameswari.bala <parameswaribala@gmail.com>
2023-10-31 12:21:53 +0530

    updated

--- azureterraformproject/go.mod
+++ azureterraformproject/go.mod
@@ -1,1 +1,5 @@
 module azureterraformproject
+
+go 1.21.3
+
+require github.com/gruntwork-io/terratest v0.46.1
--- azureterraformproject/utility/StructArray.go
+++ azureterraformproject/utility/StructArray.go
@@ -16,8 +16,9 @@ func main() {
 	insatnceTypes := [...]string{"nano", "micro", "medium", "large", "xlarge"}
 	for i := 0; i < len(vmInstances); i++ {
 		vmInstances[i] = models.VMConfiguration{
-			CPUCount:     rand.Intn(16),
-			RAM:          strconv.Itoa(rand.Intn(100)) + "GB",
+			CPUCount: rand.Intn(16),
+			RAM:      strconv.Itoa(rand.Intn(100)) + "GB",
+			//select random name not by index
 			Regions:      regions[0],
 			AccessKey:    "Aegwfu3624586235",
 			SecretKey:    "BIGIG*86869769",
