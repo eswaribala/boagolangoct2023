af3d79639a00009889381d1dc374894a9c9af951
parameswari.bala <parameswaribala@gmail.com>
2023-10-31 11:50:49 +0530

    updated

---
+++ azureterraformproject/utility/StructArray.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"azureterraformproject/models"
+	"fmt"
+	"math/rand"
+	"strconv"
+)
+
+//generate 100 vm instances
+
+func main() {
+	//declared
+	vmInstances := make([]models.VMConfiguration, 100)
+	regions := [...]string{"US-East", "US-West", "Australia", "India"}
+	for i := 0; i < len(vmInstances); i++ {
+		vmInstances[i] = models.VMConfiguration{
+			CPUCount:     rand.Intn(16),
+			RAM:          strconv.Itoa(rand.Intn(100)) + "GB",
+			Regions:      regions[0],
+			AccessKey:    "Aegwfu3624586235",
+			SecretKey:    "BIGIG*86869769",
+			InstanceType: "t2.nano",
+			Provider:     "azure",
+			VMName:       "BOA_VM" + strconv.Itoa(i),
+		}
+	}
+
+	for _, value := range vmInstances {
+		fmt.Printf("Vm Instance Details %+v\n", value)
+	}
+
+}
