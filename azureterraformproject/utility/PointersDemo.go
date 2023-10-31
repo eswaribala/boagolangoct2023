db0a89d559dc64d9d671484bbf85912c251525c1
parameswari.bala <parameswaribala@gmail.com>
2023-10-31 13:19:57 +0530

    updated

--- azureterraformproject/utility/PointersDemo.go
+++ azureterraformproject/utility/PointersDemo.go
@@ -26,8 +26,18 @@ func main() {
 	fmt.Printf("After changing VM Name=%s\n", vmName)
 	fmt.Printf("After Changing VM Name=%v\n", *pvmName)
 	fmt.Printf("After Changing Address of VM Name=%v\n", pvmName)
+	result := changeNameCallByValue(vmName)
+	fmt.Printf("After changing VM Name using call by value=%s\n", result)
 }
 
+// call by reference
 func changeName(vmName *string) {
 	*vmName = *vmName + strconv.Itoa(rand.Intn(10000000000000))
+}
+
+// call by value
+func changeNameCallByValue(vmName string) (result string) {
+	vmName1 := vmName + strconv.Itoa(rand.Intn(100))
+	fmt.Printf("Modified Value=%s\n", vmName1)
+	return vmName1
 }
