5d819c189cdd7af864ba282225df340f9aa2a1db
parameswari.bala <parameswaribala@gmail.com>
2023-10-31 12:34:55 +0530

    updated

--- azureterraformproject/utility/StructArray.go
+++ azureterraformproject/utility/StructArray.go
@@ -3,6 +3,7 @@ package main
 import (
 	"azureterraformproject/models"
 	"fmt"
+	"github.com/gruntwork-io/terratest/modules/random"
 	"math/rand"
 	"strconv"
 )
@@ -12,14 +13,14 @@ import (
 func main() {
 	//declared
 	vmInstances := make([]models.VMConfiguration, 100)
-	regions := [...]string{"US-East", "US-West", "Australia", "India"}
+	regions := []string{"US-East", "US-West", "Australia", "India"}
 	insatnceTypes := [...]string{"nano", "micro", "medium", "large", "xlarge"}
 	for i := 0; i < len(vmInstances); i++ {
 		vmInstances[i] = models.VMConfiguration{
 			CPUCount: rand.Intn(16),
 			RAM:      strconv.Itoa(rand.Intn(100)) + "GB",
 			//select random name not by index
-			Regions:      regions[0],
+			Regions:      random.RandomString(regions),
 			AccessKey:    "Aegwfu3624586235",
 			SecretKey:    "BIGIG*86869769",
 			InstanceType: insatnceTypes[rand.Intn(4)],
