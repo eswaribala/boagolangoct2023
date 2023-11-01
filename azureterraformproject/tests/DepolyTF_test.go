7d09d42f44d70e9558d003beb17174d467becdf4
parameswari.bala <parameswaribala@gmail.com>
2023-11-01 17:23:16 +0530

    updated

--- azureterraformproject/tests/DepolyTF_test.go
+++ azureterraformproject/tests/DepolyTF_test.go
@@ -7,14 +7,21 @@ import (
 )
 
 func TestDeployTFFile(t *testing.T) {
+	// Construct the terraform options with default retryable errors to handle the most common
 	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// Set the path to the Terraform code that will be tested.
 		TerraformDir: "../terraformresources",
 	})
+
+	// Clean up resources with "terraform destroy" at the end of the test.
 	defer terraform.Destroy(t, terraformOptions)
+
+	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
-	response := terraform.Output(t, terraformOptions, "result")
-	assert.Equal(t, "Rocking with TF", response)
+
+	// Run `terraform output` to get the values of output variables and check they have the expected values.
+	output := terraform.Output(t, terraformOptions, "result")
+	assert.Equal(t, "Rocking with TF", output)
 
 }
