dbac13aeff76176bb4af631298bf81afeddfeb90
parameswari.bala <parameswaribala@gmail.com>
2023-11-01 12:11:26 +0530

    updated

--- azureterraformproject/tests/GoEnv_test.go
+++ azureterraformproject/tests/GoEnv_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 	"log"
 	"os"
@@ -8,18 +9,25 @@ import (
 )
 
 func TestGoENVVariableExistence(t *testing.T) {
-	result, exists := os.LookupEnv("GOPROXY")
-	if exists {
-		assert.Equal(t, "https://proxy.golang.org,direct", result)
-	}
-
 	//log file
-	file, err := os.OpenFile("testlog", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	file, err := os.OpenFile("logs/testlog", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
 
+	result, exists := os.LookupEnv("GOPROXY")
+	if exists {
+		assert.Equal(t, "https://proxy.golang.org,direct", result)
+	}
 	log.SetOutput(file)
 	log.Println("Pushing Data to Log File..")
+	//read custom environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found...")
+	}
+	result, exists = os.LookupEnv("cloudName")
+	if exists {
+		assert.Equal(t, "AzureCloud", result)
+	}
 
 }
