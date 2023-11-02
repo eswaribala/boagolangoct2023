aeae30a259ac72712a49e9871f7a3d7687e37fe9
parameswari.bala <parameswaribala@gmail.com>
2023-11-02 15:20:36 +0530

    updated

--- azureterraformproject/utility/Routine.go
+++ azureterraformproject/utility/Routine.go
@@ -6,13 +6,13 @@ import (
 	"net/http"
 )
 
-func routineAccessLink(link *string, pChannel chan string) {
-	response, err := http.Get(*link)
+func routineAccessLink(link string, pChannel chan string) {
+	response, err := http.Get(link)
 	if err != nil {
 		log.Fatal("Site Not Available")
 	} else {
 		log.Printf("Found the site=%s and having response code%d\n",
-			*link, response.StatusCode)
+			link, response.StatusCode)
 		pChannel <- "Routine completed the task"
 	}
 
@@ -31,11 +31,12 @@ func main() {
 
 	for _, value := range links {
 		//function call converted to routine call
-		go routineAccessLink(&value, channel)
+		go routineAccessLink(value, channel)
 	}
 
-	for _, link := range links {
-		fmt.Printf("Message received from the link=%s to channel%s\n", link, <-channel)
+	for i := 0; i < 3; i++ {
+
+		fmt.Printf("Message received from  to channel%s\n", <-channel)
 	}
 
 	log.Println("Visited all the links")
