4d67718b900b9607d1dcff5910cd11a053952e07
parameswari.bala <parameswaribala@gmail.com>
2023-11-02 14:55:20 +0530

    updated

--- azureterraformproject/utility/Routine.go
+++ azureterraformproject/utility/Routine.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
-func routineAccessLink(link *string) {
+func routineAccessLink(link *string, pChannel chan string) {
 	response, err := http.Get(*link)
 	if err != nil {
 		log.Fatal("Site Not Available")
 	} else {
-		log.Printf("Found the site=%s and having response code%d",
+		log.Printf("Found the site=%s and having response code%d\n",
 			*link, response.StatusCode)
+		pChannel <- "Routine completed the task"
 	}
 
 }
@@ -24,9 +26,16 @@ func main() {
 		"https://www.rediffmail.com",
 	}
 
+	//create the channel
+	channel := make(chan string)
+
 	for _, value := range links {
 		//function call converted to routine call
-		go routineAccessLink(&value)
+		go routineAccessLink(&value, channel)
+	}
+
+	for _, link := range links {
+		fmt.Printf("Message received from the link=%s to channel%s\n", link, <-channel)
 	}
 
 	log.Println("Visited all the links")
