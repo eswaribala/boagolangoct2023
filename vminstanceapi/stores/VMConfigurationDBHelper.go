743f60f818f9540124bf1603368908b74c0a9e54
parameswari.bala <parameswaribala@gmail.com>
2023-11-03 12:18:24 +0530

    updated

--- vminstanceapi/stores/VMConfigurationDBHelper.go
+++ vminstanceapi/stores/VMConfigurationDBHelper.go
@@ -1,7 +1,9 @@
 package stores
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"log"
@@ -43,37 +45,54 @@ func CreateDBConnection() (*gorm.DB, error) {
 func SaveVMInstance(response http.ResponseWriter, request *http.Request) {
 
 	db, _ := CreateDBConnection()
-
+	var vmInstance VMConfiguration
 	tx := db.Begin()
 	db.Set("gorm:auto_preload", true)
+	json.NewDecoder(request.Body).Decode(&vmInstance)
 	db.Create(vmInstance)
 	tx.Commit()
-
+	response.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(response).Encode(vmInstance)
 }
 
 // GetAllVMInstances select all rows
 func GetAllVMInstances(response http.ResponseWriter, request *http.Request) {
 	db, _ := CreateDBConnection()
-
 	var vmInstances []VMConfiguration
 	//db.Find(&customers)
 	db.Find(&vmInstances)
+	response.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(response).Encode(vmInstances)
 
 }
 
 func GetVMConfigurationByName(response http.ResponseWriter, request *http.Request) {
 	db, _ := CreateDBConnection()
-
 	var vmInstance VMConfiguration
-
+	params := mux.Vars(request)
+	vmName := params["vmName"]
 	db.First(&vmInstance, vmName)
+	response.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(response).Encode(vmInstance)
 
 }
 
 func DeleteVMInstance(response http.ResponseWriter, request *http.Request) {
-
+	db, _ := CreateDBConnection()
+	params := mux.Vars(request)
+	vmName := params["vmName"]
+	db.Where("VMName=?", vmName).Delete(&VMConfiguration{})
+	response.WriteHeader(http.StatusOK)
 }
 
 func UpdateVMInstance(response http.ResponseWriter, request *http.Request) {
-
+	db, _ := CreateDBConnection()
+	var vmInstance VMConfiguration
+	tx := db.Begin()
+	db.Set("gorm:auto_preload", true)
+	json.NewDecoder(request.Body).Decode(&vmInstance)
+	db.Save(vmInstance)
+	tx.Commit()
+	response.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(response).Encode(vmInstance)
 }
