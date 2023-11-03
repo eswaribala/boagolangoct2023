665cc4a605f1fd31cb7f318b5ef8464022ef9599
parameswari.bala <parameswaribala@gmail.com>
2023-11-03 13:19:22 +0530

    updated

--- vminstanceapi/main.go
+++ vminstanceapi/main.go
@@ -27,11 +27,11 @@ func main() {
 	router := mux.NewRouter()
 
 	//create api endpoints
-	router.HandleFunc("/vminstances/v1.0", stores.SaveVMInstance).Methods("POST")
-	router.HandleFunc("/vminstances/v1.0", stores.GetAllVMInstances).Methods("GET")
-	router.HandleFunc("/vminstances/v1.0/{vmName}", stores.GetVMConfigurationByName).Methods("GET")
-	router.HandleFunc("/vminstances/v1.0/{vmName}", stores.DeleteVMInstance).Methods("DELETE")
-	router.HandleFunc("/vminstances/v1.0", stores.UpdateVMInstance).Methods("PUT")
+	router.HandleFunc("/vminstances", stores.SaveVMInstance).Methods("POST")
+	router.HandleFunc("/vminstances", stores.GetAllVMInstances).Methods("GET")
+	router.HandleFunc("/vminstances/{vmName}", stores.GetVMConfigurationByName).Methods("GET")
+	router.HandleFunc("/vminstances/{vmName}", stores.DeleteVMInstance).Methods("DELETE")
+	router.HandleFunc("/vminstances", stores.UpdateVMInstance).Methods("PUT")
 
 	stores.CreateDBConnection()
 	//swagger documentation
