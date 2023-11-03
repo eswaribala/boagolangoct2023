3fbb34188de0bb303fff0cabc69e446d80e38b3c
parameswari.bala <parameswaribala@gmail.com>
2023-11-03 12:31:18 +0530

    updated

--- vminstanceapi/main.go
+++ vminstanceapi/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	_ "github.com/swaggo/files"
+	_ "github.com/swaggo/gin-swagger"
+	_ "github.com/swaggo/http-swagger"
+	"log"
+	"net/http"
 	"vminstanceapi/stores"
 )
 
@@ -15,5 +20,10 @@ func main() {
 	router.HandleFunc("/vminstances/v1.0/{vmName}", stores.GetVMConfigurationByName).Methods("GET")
 	router.HandleFunc("/vminstances/v1.0/{vmName}", stores.DeleteVMInstance).Methods("DELETE")
 	router.HandleFunc("/vminstances/v1.0", stores.UpdateVMInstance).Methods("PUT")
+
+	//swagger documentation
+
+	//api deployment
+	log.Fatal(http.ListenAndServe("7074", router))
 
 }
